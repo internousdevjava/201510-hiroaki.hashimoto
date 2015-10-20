ca67751db953243bab7e3a05696d88180324293a
internous <internous@00174045469704>
2015-10-20 16:27:05 +0900

    基礎課題2（修正済み）

--- src/Kisokadai1.java
+++ src/Kisokadai1.java
@@ -9,46 +9,31 @@ import java.io.BufferedReader;
 
 			    int x;
 	            int y;
-	            
+	            try{
 	            // x入力
 	            System.out.print("xを入力してください: ");
 	            InputStreamReader isrx = new InputStreamReader(System.in);
-	            BufferedReader brx = new BufferedReader(isrx);
-	            try{
-	                    String bufx = brx.readLine();
-	                    x = Integer.parseInt(bufx);
-	            }catch(Exception e){
-	                    x = 0;
-	            }
+	            BufferedReader bufx = new BufferedReader(isrx);
+	                    String str = bufx.readLine();
+	                    x = Integer.parseInt(str);
 	         
 	            //　y入力
 	            System.out.print("yを入力してください: ");
-	            InputStreamReader isry = new InputStreamReader(System.in);
-	            BufferedReader bry = new BufferedReader(isry);
-	            try{
-	                String bufy = bry.readLine();
-	                y = Integer.parseInt(bufy);
-	            }catch(Exception e){
-	                y = 0;
-	            }
-	            
-			    // 配列の宣言。
-			    int kuku[][] = new int[x][y];
-
-			    // 九九の結果を配列に保存。
-			    for( int i=0; i<x; i++ ) {
-			      for( int j=0; j<y; j++ ){
-			    	  kuku[i][j] = (i+1) * (j+1);
-			      }
-			    }
+	         
+	            y = Integer.parseInt(bufx.readLine());
 
 			    // 九九の結果を表示。
-			    for( int i=0; i<x; i++ ) {
-			      for( int j=0; j<y; j++ ) { 
-			    	  System.out.print(kuku[i][j] + " "); 
-			      }
-			      	  System.out.println(); 
+			    for( int i=1; i<x; i++ ) {
+			      for( int j=1; j<y; j++ ) { 
+			    	  System.out.print(i*j ); 
+			      
+			      	  System.out.println(""); 
 			    }
+	            }
+	            }
+			    catch(Exception e){
+                    System.err.println(e);
+            }
 		  }
 	}
 
